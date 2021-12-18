607df3d68ff2d5dd761eaa845cff54239747bf64
bmoazen <brianmoazen@gmail.com>
2021-12-18 17:29:57 -0500

    update queries

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -25,7 +25,6 @@ FROM retirement_info
 LEFT JOIN dept_employee
 ON retirement_info.emp_no = dept_employee.emp_no;
 
--- Create table of current employees eligible for retirement
 SELECT ri.emp_no,
     ri.first_name,
     ri.last_name,
@@ -35,3 +34,57 @@ FROM retirement_info as ri
 LEFT JOIN dept_employee as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employee as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+--List of retiring employees with salaries
+SELECT e.emp_no, e.first_name, e.last_name, e.gender,
+		s.salary, 
+		de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no=s.emp_no)
+INNER JOIN dept_employee as de
+ON (e.emp_no=de.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+		
+SELECT * FROM manager_info
+
+
+SELECT ce.emp_no, ce.first_name, ce.last_name,
+		d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employee AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
