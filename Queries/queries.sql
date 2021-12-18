9f30f00e06c790d8dc33e42bfa219f2350f94b6b
bmoazen <brianmoazen@gmail.com>
2021-12-18 17:12:05 -0500

    created current_employees.csv and queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,13 +1,37 @@
--- Retirement Eligibility
-SELECT COUNT(first_name)
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT first_name, last_name
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
 
-SELECT * FROM retirement_info
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_employee.to_date
+FROM retirement_info
+LEFT JOIN dept_employee
+ON retirement_info.emp_no = dept_employee.emp_no;
+
+-- Create table of current employees eligible for retirement
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employee as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
